6cfeb8f1665c33969b36afffafdabe4e9c245816
CamiloEstebanConchaTorres <156373519+CamiloEstebanConchaTorres@users.noreply.github.com>
2024-04-19 12:39:29 -0500

    feat: :construction: proceso de levantamiento de Base de Datos
    se realizo el proceso de devolver datos de un JSON en un hosting creado previamente, se corrigieron errores de estructura y sintaxis tambien

--- js/bill.js
+++ js/bill.js
@@ -1,2 +1,18 @@
-import {example} from "./component/shopBillCamper.js"
-console.log(example())
+import {getAllProductBIll} from "./component/shopBillCamper.js"
+let myBill = document.querySelector("#myBill");
+let data = await getAllProductBIll();
+let row = ""
+
+data.forEach((val, id)=>{
+    val.products.forEach(product =>{
+        row += `
+            <tr>
+                <td>${product.description}</td>
+                <td>${product.quantity}</td>
+                <td>${product.price}</td>
+                <td>${product.total}</td>
+            </tr>
+            `
+    })
+})
+myBill.innerHTML = row
--- js/component/shopBillCamper.js
+++ js/component/shopBillCamper.js
@@ -1,3 +1,5 @@
-export const example = () =>{
-    return "Helloword"
+export const getAllProductBIll = async() =>{
+    let conexion = await fetch("http://localhost:5600/camper");
+    let data = await conexion.json();
+    return data;
 }
