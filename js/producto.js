31153246fb3b841fd08a7af95f796bfcec76b375
CamiloEstebanConchaTorres <156373519+CamiloEstebanConchaTorres@users.noreply.github.com>
2024-04-22 12:48:13 -0500

    feat: :construction: otro ejemplo de destructuracion de datos con recursividad

--- js/producto.js
+++ js/producto.js
@@ -1,7 +1,17 @@
 import fs from 'node:fs';
-fs.readFile(import.meta.dirname+"/db/product.json", 'utf-8', (err, data) => {
+fs.readFile(import.meta.dirname+"/db/product.json", "utf-8", (err, data)=>{
     let json = JSON.parse(data);
-    let [producto1] = json.products;
-    let {description: des} = producto1
-    console.log(des);
-});
+
+    let contar = (tamano=0, nombreGama, array=[])=>{
+        let productos = (json.products.length != tamano) ? json.products[tamano] : undefined;
+        let gamaNuevo;
+        try {
+            gamaNuevo = `${(productos.gama != nombreGama) ? productos.name : undefined}`
+        } catch (error) {
+            gamaNuevo = undefined;
+        }
+        array.push(gamaNuevo)
+        return (json.products.length == tamano) ? [...new Set(array)] : contar(tamano+1, "Frutales", array) ;
+    } 
+    console.log(contar());
+})
