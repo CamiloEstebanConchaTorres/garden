60e0d71007d92f4ea07981af73353e74b6e24908
CamiloEstebanConchaTorres <156373519+CamiloEstebanConchaTorres@users.noreply.github.com>
2024-04-23 12:31:48 -0500

    feat: :construction: Solucion de ejercicios del 1 al 5 para realizar filtracion de tablas
    se realizo la filtracion de lo solicitado en los ejercicios del README.md

---
+++ js/main.js
@@ -0,0 +1,12 @@
+import {
+    getAllOficcesCOdeAndCity,
+    getAllOficcesFromSpainCityAndMovil
+} from "../module/offices.js";
+
+import {
+    getAllEmployeersWithBossCODE7,
+    getBOSS,
+    getAll
+} from "../module/employees.js";
+
+console.log(await getBOSS());
